39adeb22c56cd7e594d29082bfeda7eb354e4003
刘阔 <278960878@qq.com>
2016-07-17 09:38:24 +0800

    fix 脊柱宝宝 架构提交

---
+++ BB4Car/src/main/java/com/nice/jzb/ui/main/MainHomeFragmentNew.java
@@ -0,0 +1,16 @@
+package com.nice.jzb.ui.main;
+
+import com.nice.jzb.R;
+import com.nice.jzb.core.AbstractFragment;
+
+import org.androidannotations.annotations.EFragment;
+
+/**
+ * Created by nice on 16/3/16.
+ */
+
+@EFragment(R.layout.fragment_main_home)
+public class MainHomeFragmentNew extends AbstractFragment {
+
+
+}
---
+++ BB4Car/src/main/java/com/nice/jzb/ui/main/MeFragment.java
@@ -0,0 +1,17 @@
+package com.nice.jzb.ui.main;
+
+import com.nice.jzb.R;
+import com.nice.jzb.core.AbstractFragment;
+
+import org.androidannotations.annotations.EFragment;
+
+/**
+ * Created by sufun_job on 2016/2/16.
+ *
+ * @description 用户个人主页`
+ */
+@EFragment(R.layout.setting_me_layout)
+public class MeFragment extends AbstractFragment {
+
+
+}
